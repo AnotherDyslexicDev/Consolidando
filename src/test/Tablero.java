b88165d44feab8a0f682ddc5d90f4b40b420ed92
carlos <carlosbootcampunab@gmail.com>
2023-03-05 23:38:16 -0300

    .

--- src/test/Tablero.java
+++ src/test/Tablero.java
@@ -10,12 +10,10 @@ import java.util.Random;
 
 public class Tablero {
 
-	private char[][] matriz;
+	char[][] Matriz = new char[15][15];
+	Carro[] listaCarros = new Carro[18];
 	
-	public Tablero() {
-		super();
-		this.matriz =  new char[15][15];
-	}
+
 	ArrayList<Kromis>listaKromis = new ArrayList<Kromis>();
 	public void generarKromis() {
 	    Random random = new Random();   
@@ -55,23 +53,23 @@ public class Tablero {
 	    // Crear las tres Kromis 
 	    Kromis k1 = new Kromis(50, 1990, fila1, columna1, 2023, "Kromi");
 	    listaKromis.add(k1);
-	    matriz[fila1][columna1] = 'K';
-	    matriz[fila1+1][columna1] = 'K';
-	    matriz[fila1+2][columna1] = 'K';
+	    Matriz[fila1][columna1] = 'K';
+	    Matriz[fila1+1][columna1] = 'K';
+	    Matriz[fila1+2][columna1] = 'K';
 	    
 	    
 	    Kromis k2 = new Kromis(10, 1995, fila2, columna2, 2023, "Kromi");
 	    listaKromis.add(k2);
-	    matriz[fila2][columna2] = 'K';
-	    matriz[fila2+1][columna2] = 'K';
-	    matriz[fila2+2][columna2] = 'K';
+	    Matriz[fila2][columna2] = 'K';
+	    Matriz[fila2+1][columna2] = 'K';
+	    Matriz[fila2+2][columna2] = 'K';
 	    
 	    
 	    Kromis k3 = new Kromis(7, 1992, fila3, columna3, 2023, "Kromi");
 	    listaKromis.add(k3);
-	    matriz[fila3][columna3] = 'K';
-	    matriz[fila3+1][columna3] = 'K';
-	    matriz[fila3+2][columna3] = 'K';
+	    Matriz[fila3][columna3] = 'K';
+	    Matriz[fila3+1][columna3] = 'K';
+	    Matriz[fila3+2][columna3] = 'K';
 
 	    
 	}
@@ -88,16 +86,16 @@ public class Tablero {
 		
 		Huevo h = new Huevo(x,y);
 		listaHuevos.add(h);
-		matriz[x][y] = 'h';
+		Matriz[x][y] = 'h';
 		System.out.println("huevo:" + x);
 		System.out.println("huevo:" + y);
 	}
 	
 	//imprime linea matriz
-	public void mostrarMatriz() {
-		for (int i= 0; i < matriz.length; i++) {
-			for (int j= 0; j < matriz[0].length; j++) {
-				System.out.print(matriz[i][j] + " ");				
+	public void mostrarTablero() {
+		for (int i= 0; i < Matriz.length; i++) {
+			for (int j= 0; j < Matriz[0].length; j++) {
+				System.out.print(Matriz[i][j] + " ");				
 			}
 			System.out.println();
 		}
@@ -110,7 +108,7 @@ public class Tablero {
 		
 		char m = '-';
 		
-		for (char[] row : tablero.matriz) {
+		for (char[] row : tablero.Matriz) {
 			Arrays.fill(row, m);  
 		}
 		
@@ -119,7 +117,7 @@ public class Tablero {
 		
 		
 		tablero.generarKromis();
-		tablero.mostrarMatriz();
+		tablero.mostrarTablero();
 	}
 	
 	
