d8baa4f5f10d89d3c0847077b66871d020b45d27
Carlos Carrasco A <121955479+CarlosDavid1993@users.noreply.github.com>
2023-03-09 02:36:30 -0300

    javadoc

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"test","l":"Caguano"},{"p":"test","l":"Carro"},{"p":"test","l":"Huevo"},{"p":"test","l":"Kromi"},{"p":"test","l":"Main"},{"p":"test","l":"Tablero"},{"p":"test","l":"Trupalla"}];updateSearchResults();
